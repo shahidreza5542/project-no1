7655b22c79fefcebda05e11f0285e31ad7746850
shahidreza <shahidreza660@gmail.com>
2025-08-11 18:59:07 +0530

    refactor: update index.html structure and add script.js for color switching functionality

---
+++ script.js
@@ -0,0 +1,28 @@
+const button = document.querySelectorAll('.button')
+const body = document.querySelector('body')
+
+button.forEach( function(button) {
+    console.log(button);
+    button.addEventListener('click', function(event){
+        console.log(event);
+        console.log(event.target);
+        if (event.target.id === 'grey') {
+            body.style.backgroundColor = 'grey'
+        }
+
+        if (event.target.id === 'white') {
+            body.style.backgroundColor = 'white'
+        }
+
+        if (event.target.id === 'blue') {
+            body.style.backgroundColor = 'blue'
+        }
+
+        if (event.target.id === 'yellow') {
+            body.style.backgroundColor = 'yellow'
+        }
+        
+        
+    })
+    
+} )
